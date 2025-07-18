cc4dff3c11c843f29e8a6804c31b877672a1e83f
Sergey Martynov <sergey.martynov@dostavista.ru>
2025-07-18 20:29:20 +0300

    Update Parser.php

--- src/Parser.php
+++ src/Parser.php
@@ -1,22 +1,23 @@
 <?php
-
 namespace Differ\Parser;
+
+class ParseError extends \Exception {}
 
 function parseFile(string $filepath): array
 {
     $realPath = realpath($filepath);
     if ($realPath === false) {
-        throw new \Exception("File not found: {$filepath}");
+        throw new ParseError("File not found: {$filepath}");
     }
 
     $content = file_get_contents($realPath);
     if ($content === false) {
-        throw new \Exception("Can't read file: {$filepath}");
+        throw new ParseError("Can't read file: {$filepath}");
     }
 
     $data = json_decode($content, true);
     if ($data === null && json_last_error() !== JSON_ERROR_NONE) {
-        throw new \Exception("Invalid JSON in file: {$filepath}");
+        throw new ParseError("Invalid JSON in file: {$filepath}");
     }
 
     return $data;
